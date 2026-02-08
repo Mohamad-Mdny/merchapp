6f8c6e2fd52a2b105895dcdd163cac928d6bd6d8
Mohamad <182977298+Mohamad-Mdny@users.noreply.github.com>
2026-02-08 22:57:05 +0000

    hello

--- src/main/java/org/example/merchapp/Launcher.java
+++ src/main/java/org/example/merchapp/Launcher.java
@@ -3,7 +3,13 @@ package org.example.merchapp;
 import javafx.application.Application;
 
 public class Launcher {
+    String hello = "Hello";
     public static void main(String[] args) {
         Application.launch(HelloApplication.class, args);
     }
+    
+    public void hellos{
+        System.out.println(hello);
+    }
+
 }
