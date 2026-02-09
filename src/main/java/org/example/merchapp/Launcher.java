2d3e87a8050182d898757220a91db361b271fed4
Mohamad <182977298+Mohamad-Mdny@users.noreply.github.com>
2026-02-09 03:13:10 +0000

    css sheets

--- src/main/java/org/example/merchapp/Launcher.java
+++ src/main/java/org/example/merchapp/Launcher.java
@@ -3,13 +3,7 @@ package org.example.merchapp;
 import javafx.application.Application;
 
 public class Launcher {
-    String hello = "Hello";
     public static void main(String[] args) {
         Application.launch(HelloApplication.class, args);
     }
-
-    public void hellos{
-        System.out.println(hello);
-    }
-
 }
