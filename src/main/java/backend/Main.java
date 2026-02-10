c92451eeebfc5f1cc1842874181148d76d2614c5
Mohamad <182977298+Mohamad-Mdny@users.noreply.github.com>
2026-02-10 05:24:46 +0000

    finished login and register

--- src/main/java/backend/Main.java
+++ src/main/java/backend/Main.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/merchappfrontend/Register.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/merchappfrontend/Login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 500);
         stage.setTitle("Merchant App");
         stage.setScene(scene);
